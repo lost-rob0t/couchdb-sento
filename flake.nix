f104ea2a7648a5ebd65e1a6c64afda640851593f
N545PY <nsaspy@fedora.email>
2024-10-29 06:44:22 -0400

    add flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A modern Couchdb 3.x Client. but with actors and agents. seeds for a service.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    devShell.x86_64-linux =
+      pkgs.mkShell {
+        buildInputs = with pkgs; [
+          pkg-config
+          roswell
+          sbcl
+
+          # normally stuff goes in here
+        ];
+        shellHook = ''
+              export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath([pkgs.openssl])}
+            '';
+      };
+  };
+}
